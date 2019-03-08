500b39dda4b6a57319c177e6c8476eb86f2bd4f8
glaplant <glaplant@byui.edu>
2019-03-07 23:55:00 -0600

    json

---
+++ assignments/lesson-8/scripts/windchill.js
@@ -0,0 +1,8 @@
+var t =;
+var s =;
+
+t=(air average temperature)
+s= windspeed in mph
+var f=35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow( s,0.16));
+
+document.getElementById("windchill").innerHTML = f;
