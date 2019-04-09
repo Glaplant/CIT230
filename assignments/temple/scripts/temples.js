15ceed677afca120c039e4ce554d50ed3bdf94be
glaplant <glaplant@byui.edu>
2019-04-09 07:37:15 -0500

    json

--- assignments/temple/scripts/temples.js
+++ assignments/temple/scripts/temples.js
@@ -11,6 +11,10 @@ templeRequest.send();
     console.log(templeData);
 
 
-    document.getElementById("name2").innerHTML= templeData.temples[0].name;
+    document.getElementById("name1").innerHTML= templeData.temples[0].name;
+    document.getElementById("name2").innerHTML= templeData.temples[1].name;
+    document.getElementById("name3").innerHTML= templeData.temples[2].name;
+    document.getElementById("name4").innerHTML= templeData.temples[3].name;
+
 
     }
