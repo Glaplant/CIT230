5c137c6c0001078ee900647acc848a043d169824
glaplant <glaplant@byui.edu>
2019-04-08 23:52:53 -0500

    updates

--- assignments/temple/scripts/temples.js
+++ assignments/temple/scripts/temples.js
@@ -1,13 +1,16 @@
 let templeRequest = new XMLHttpRequest();
-let templeURLstring = "http//github/json/templedata.json";
+let templeURLstring = "http://glaplant.github.io/CIT230//assignments/temple/json/templedata.json";
 templeRequest.open('GET' , templeURLstring, true);
 templeRequest.send();
 
 
 
 
-    templeRequest1.onload = function() {
+    templeRequest.onload = function() {
     let templeData = JSON.parse(templeRequest.responseText);
     console.log(templeData);
+
+
+    document.getElementById('name2').innerHTML=templeData.temples[0].name;
 
     }
