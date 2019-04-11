5ab4ea70a2fdff39eb52376ae8a8413138257cb4
glaplant <glaplant@byui.edu>
2019-04-10 20:28:03 -0500

    updates

--- assignments/temple/scripts/temples.js
+++ assignments/temple/scripts/temples.js
@@ -32,105 +32,123 @@ templeRequest.send();
    var scheduleNum;
 
     for(t=0; t < templeData.temples.length; t++){
+
+     var templeNum = [t];
     
-    switch (t){
+    switch (templeNum){
 
          case 0 :
-         for(h=0;h < templeData.temples[t].history.length; h++){
-            document.getElementsByClassName("history1").innerHTML = templeData.temples[t].history[h];
-        }
+            
+              for(h=0;h < templeData.temples[templeNum].history.length; h++){
+                   historyNum =[h];
+                   document.getElementsByClassName("history1").innerHTML = templeData.temples[templeNum].history[historyNum];
+            }
 
-      for(s=0;s < templeData.temples[t].services.length; s++){
-            document.getElementsByClassName("services1").innerHTML=templeData.temples[t].services[s];
-        }
+              for(s=0;s < templeData.temples[templeNum].services.length; s++){
+                    servicesNum= [s];
+                    document.getElementsByClassName("services1").innerHTML=templeData.temples[templeNum].services[servicesNum];
+              }
 
-     for(o=0;o < templeData.temples[t].ordinances.length; o++){
-            document.getElementsByClassName("ordinances1").innerHTML=templeData.temples[t].ordinances[o];
-        }
+              for(o=0;o < templeData.temples[templeNum].ordinances.length; o++){
+                   ordinancesNum = [o];
+                   document.getElementsByClassName("ordinances1").innerHTML=templeData.temples[templeNum].ordinances[ordinancesNum];
+                }
 
-      for(c=0;c < templeData.temples[t].closures.length; c++){
-            document.getElementsByClassName("closures1").innerHTML= templeData.temples[t].closures[c];
+              for(c=0;c < templeData.temples[templeNum].closures.length; c++){
+                    closuresNum = [c];
+                    document.getElementsByClassName("closures1").innerHTML= templeData.temples[templeNum].closures[closuresNum];
 
-        }
+              }
 
-     for(e=0; e < templeData.temples[t].schedule.length; e++){
-            document.getElementsByClassName("schedule1").innerHTML= "Endowment Session" + templeData.temples[t].schedule[e];
-        }
+              for(e=0; e < templeData.temples[templeNum].schedule.length; e++){
+
+                scheduleNum = [e];
+                document.getElementsByClassName("schedule1").innerHTML= "Endowment Session" + templeData.temples[templeNum].schedule[scheduleNum];
+              }
     
         case 1 :
 
-            for(h=0;h < templeData.temples[t].history.length; h++){
-                  document.getElementsByClassName("history2").innerHTML = templeData.temples[t].history[h];
+            for(h=0;h < templeData.temples[templeNum].history.length; h++){
+                  historyNum =[h];
+                  document.getElementsByClassName("history2").innerHTML = templeData.temples[templeNum].history[historyNum];
               }
 
-            for(s=0;s < templeData.temples[t].services.length; s++){
-                  document.getElementsByClassName("services2").innerHTML=templeData.temples[t].services[s];
+            for(s=0;s < templeData.temples[templeNum].services.length; s++){
+                  servicesNum= [s];
+                  document.getElementsByClassName("services2").innerHTML=templeData.temples[templeNum].services[servicesNum];
               }
 
-           for(o=0;o < templeData.temples[t].ordinances.length; o++){
-                  document.getElementsByClassName("ordinances2").innerHTML=templeData.temples[t].ordinances[o];
+           for(o=0;o < templeData.temples[templeNum].ordinances.length; o++){
+                  ordinancesNum = [o];
+                  document.getElementsByClassName("ordinances2").innerHTML=templeData.temples[templeNum].ordinances[ordinancesNum];
               }
 
-            for(c=0;c < templeData.temples[t].closures.length; c++){
-                  document.getElementsByClassName("closures2").innerHTML= templeData.temples[t].closures[c];
+            for(c=0;c < templeData.temples[templeNum].closures.length; c++){
+                  closuresNum = [c];
+                  document.getElementsByClassName("closures2").innerHTML= templeData.temples[templeNum].closures[closuresNum];
 
               }
 
-           for(e=0; e < templeData.temples[t].schedule.length; e++){
-                  document.getElementsByClassName("schedule2").innerHTML= "Endowment Session" + templeData.temples[t].schedule[e];
+           for(e=0; e < templeData.temples[templeNum].schedule.length; e++){
+                  scheduleNum = [e];
+                  document.getElementsByClassName("schedule2").innerHTML= "Endowment Session" + templeData.temples[templeNum].schedule[scheduleNum];
               }
             
     
     
         case 2 :
 
-        for(h=0;h < templeData.temples[t].history.length; h++){
-            document.getElementsByClassName("history3").innerHTML = templeData.temples[t].history[h];
-        }
+            
+              for(h=0;h < templeData.temples[templeNum].history.length; h++){
+                  historyNum =[h];
+                  document.getElementsByClassName("history3").innerHTML = templeData.temples[templeNum].history[historyNum];
+                  }
 
-      for(s=0;s < templeData.temples[t].services.length; s++){
-            document.getElementsByClassName("services3").innerHTML=templeData.temples[t].services[s];
-        }
+              for(s=0;s < templeData.temples[templeNum].services.length; s++){
+                  servicesNum= [s];
+                  document.getElementByClassName("services3").innerHTML=templeData.temples[templeNum].services[servicesNum];
+                  }
 
-     for(o=0;o < templeData.temples[t].ordinances.length; o++){
-            document.getElementsByClassName("ordinances3").innerHTML=templeData.temples[t].ordinances[o];
-        }
+              for(o=0;o < templeData.temples[templeNum].ordinances.length; o++){
+                  ordinancesNum = [o];
+                  document.getElementsByClassName("ordinances3").innerHTML=templeData.temples[templeNum].ordinances[ordinancesNum];
+                  }
 
-      for(c=0;c < templeData.temples[t].closures.length; c++){
-            document.getElementsByClassName("closures3").innerHTML= templeData.temples[t].closures[c];
-
-        }
-
-     for(e=0; e < templeData.temples[t].schedule.length; e++){
-            document.getElementsByClassName("schedule3").innerHTML= "Endowment Session" + templeData.temples[t].schedule[e];
-        }
+              for(c=0;c < templeData.temples[templeNum].closures.length; c++){
+                  closuresNum = [c];
+                  document.getElementsByClassName("closures3").innerHTML= templeData.temples[templeNum].closures[closuresNum];
+                   }
+              for(e=0; e < templeData.temples[templeNum].schedule.length; e++){
+                   scheduleNum = [e];
+                   document.getElementsByClassName("schedule3").innerHTML= "Endowment Session" + templeData.temples[templeNum].schedule[scheduleNum];
+                  }
     
         case 3 :
          
-        for(h=0;h < templeData.temples[t].history.length; h++){
-            document.getElementsByClassName("history4").innerHTML = templeData.temples[t].history[h];
-        }
-
-      for(s=0;s < templeData.temples[t].services.length; s++){
-            document.getElementsByClassName("services4").innerHTML=templeData.temples[t].services[s];
-        }
-
-     for(o=0;o < templeData.temples[t].ordinances.length; o++){
-            document.getElementsByClassName("ordinances4").innerHTML=templeData.temples[t].ordinances[o];
-        }
-
-      for(c=0;c < templeData.temples[t].closures.length; c++){
-            document.getElementsByClassName("closures4").innerHTML= templeData.temples[t].closures[c];
-
-        }
-
-     for(e=0; e < templeData.temples[t].schedule.length; e++){
-            document.getElementsByClassName("schedule4").innerHTML= "Endowment Session" + templeData.temples[t].schedule[e];
-        }
+              for(h=0;h < templeData.temples[templeNum].history.length; h++){
+                  historyNum =[h];
+                  document.getElementsByClassName("history4").innerHTML = templeData.temples[templeNum].history[historyNum];
+                 }
+              for(s=0;s < templeData.temples[templeNum].services.length; s++){
+                servicesNum= [s];
+                document.getElementsByClassName("services4").innerHTML=templeData.temples[templeNum].services[servicesNum];
+                 }
+             for(o=0;o < templeData.temples[templeNum].ordinances.length; o++){
+                ordinancesNum = [o];
+                document.getElementsByClassName("ordinances4").innerHTML=templeData.temples[templeNum].ordinances[ordinancesNum];
+                }
+            for(c=0;c < templeData.temples[templeNum].closures.length; c++){
+                closuresNum = [c];
+                document.getElementsByClassName("closures4").innerHTML= templeData.temples[templeNum].closures[closuresNum];
+                }
+            for(e=0; e < templeData.temples[templeNum].schedule.length; e++){
+                scheduleNum = [e];
+                document.getElementsByClassName("schedule4").innerHTML= "Endowment Session" + templeData.temples[templeNum].schedule[scheduleNum];
+                }
             
         }
     }
-}
+    
 
 
 
